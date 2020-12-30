b32b69f071b4d6e4eb74f74ed9981c4a7b7caccd
Tarun-Karkera <76775670+Tarun-Karkera@users.noreply.github.com>
2020-12-30 17:17:41 +0530

    Add files via upload

---
+++ pharmacy/Pharmacy DBMS Project/cart.php
@@ -0,0 +1,21 @@
+<?php
+//include header.php file
+ob_start();
+include('Header.php');
+?>
+
+<?php
+/* include top sale section */
+include('Template/_cart-template.php');
+/* include top sale section */
+
+/* include top sale section */
+include('Template/_new-medicines.php');
+/* include top sale section */
+?>
+
+
+<?php
+//include footer.php file
+include('Footer.php');
+?>
